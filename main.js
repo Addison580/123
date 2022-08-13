a99a303ced2ff299326f536260ebe5d17c0becc6
Addison580 <82746288+Addison580@users.noreply.github.com>
2022-08-13 20:10:36 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+function setup(){
+    canvas=createCanvas(600, 500);
+    canvas.position(900, 100);
+    video=createCapture(VIDEO);
+    video.size(600, 500);
+    video.position(100, 100);
+    }
+    
